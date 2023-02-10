fa4c1fe9ac70ee6389fadd2f7e9ce615d9b923fb
wakisaka302 <natirouga32@gmail.com>
2023-02-10 09:32:01 +0900

    aaa

---
+++ SpringWork2/src/main/java/com/example/demo/SpringWork2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWork2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWork2Application.class, args);
+	}
+
+}
